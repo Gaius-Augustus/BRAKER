15782a715808f3b70043f986e1c6be60e75156f2
Katharina Hoff <katharina.hoff@gmail.com>
2018-05-18 06:52:36 +0000

    updated usage information for summarize_hints.pl
    git-svn-id: file:///home/gumz3/Documents/Braker/svn/braker/trunks@744 52e7708f-445d-428a-8816-bceb3badd3a6

--- scripts/auxiliary/summarize_hints.pl
+++ scripts/auxiliary/summarize_hints.pl
@@ -4,6 +4,20 @@
 # E-Mail: katharina.hoff@uni-greifswald.de
 # Date: May 16th 2018
 
+# This script was developed on request of a braker user who had generated a asn
+# file with ProSplign, converted that to gff-format and wanted to summarize
+# multiplicity information over that gff-file for intron hints. This output
+# is compatible with braker.pl, but not directly with gmes_petap.pl (GeneMark-EX).
+
+# Input format example:
+# seq1	ProSplign	exon	12839	13668	.	-	.	grp=prot1;pri=4;src=P
+# seq1	ProSplign	intron	12752	12835	.	-	.	grp=prot1;pri=4;src=P
+# seq1	ProSplign	intron	12752	12835	.	-	.	grp=prot1;pri=4;src=P
+
+# Output format example:
+# seq1	ProSplign	intron	12752	12835	.	-	.	mult=1;pri=4;src=P
+# seq1	ProSplign	intron	12752	12835	.	-	.	mult=1;pri=4;src=P
+
 # usage: cat hintsfile | summarize_hints.pl > summarized_hintsfile
 
 my %hints;
