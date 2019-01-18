849fa741ddec9ef622150fb74d817546e5181ef1
Katharina Hoff <katharina.hoff@gmail.com>
2019-01-18 18:05:02 +0100

    added --cleanup to test scripts

--- example/tests/test7.sh
+++ example/tests/test7.sh
@@ -4,4 +4,4 @@ if [ -d $wd ]; then
     rm -r $wd
 fi
 
-( time braker.pl --genome=../genome.fa --bam=../RNAseq.bam --species=fly --skipAllTraining --softmasking --workingdir=$wd ) &> test7.log
+( time braker.pl --genome=../genome.fa --bam=../RNAseq.bam --species=fly --skipAllTraining --softmasking --workingdir=$wd --cleanup ) &> test7.log
