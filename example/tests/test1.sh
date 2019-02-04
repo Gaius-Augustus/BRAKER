faec8dbf568f757619d8bb24f70c6a884e32cb53
Levi Baber <baberlevi@gmail.com>
2019-02-04 15:15:09 -0600

    fix missing parenthesis

--- example/tests/test1.sh
+++ example/tests/test1.sh
@@ -4,4 +4,4 @@ if [ -d $wd ]; then
     rm -r $wd
 fi
 
- time braker.pl --genome=../genome.fa --bam=../RNAseq.bam --softmasking --workingdir=$wd --cleanup ) &> test1.log
+( time braker.pl --genome=../genome.fa --bam=../RNAseq.bam --softmasking --workingdir=$wd --cleanup ) &> test1.log
