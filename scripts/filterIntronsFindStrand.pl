66ac89d2cfb9f186eb0417ae49ce4d3770b555a1
Katharina Hoff <katharina.hoff@gmail.com>
2019-12-11 11:06:13 +0100

    filterIntronsFindStrand.pl : do not strand hints that already have a strand

--- scripts/filterIntronsFindStrand.pl
+++ scripts/filterIntronsFindStrand.pl
@@ -5,11 +5,11 @@
 # filterIntronsFindStrand.pl - finds corresponding strand for introns in fasta file                #
 #                              optionally set the score column to the 'mult' entry with --score    #
 #                                                                                                  #
-# Author: Simone Lange                                                                             #
+# Author: Simone Lange & Katharina J. Hoff                                                         #
 #                                                                                                  #
 # Contact: katharina.hoff@uni-greifswald.de                                                        #
 #                                                                                                  #
-# Release date: January 7th 2015                                                                   #
+# Release date: December 11th 2019                                                                 #
 #                                                                                                  #
 # This script is under the Artistic Licence                                                        #
 # (http://www.opensource.org/licenses/artistic-license.php)                                        #
@@ -22,6 +22,8 @@
 # | add error message if sequence     |                |23.01.2015 |
 # | name of hints and fasta file do   |                |           |
 # | not match -> program   stops then |                |           |
+# | excempt already stranded hints    | Katharina Hoff |11.12.2019 |
+# | output both strands if ambigous   |                |           |
 # ------------------------------------------------------------------
 
 use strict;
@@ -135,16 +137,23 @@ while(<INTRONS>){
     chomp;
     my @line = split(/\t/, $_);
     if(scalar(@line) == 9){
-    my $strand = findStrand($line[0], $line[3], $line[4]);
-      if( $strand eq "+" || $strand eq "-") {
-          my $score;
-          if($mult_score){
-              $score = getScore($line[8]);
-          }else{
-              $score = $line[5];
-          }
-          print "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$score\t$strand\t$line[7]\t$line[8]\n";
-      }
+        if(not($line[6] =~ m/\./)){
+            my $strand = findStrand($line[0], $line[3], $line[4]);
+            if( $strand eq "+" || $strand eq "-" || $strand eq "b" ) {
+                my $score;
+                if($mult_score){
+                    $score = getScore($line[8]);
+                }else{
+                    $score = $line[5];
+                }
+                if($strand eq "+" || $strand eq "-"){
+                    print "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$score\t$strand\t$line[7]\t$line[8]\n";
+                }elsif($strand eq "b"){
+                    print "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$score\t+\t$line[7]\t$line[8]\n";
+                    print "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]\t$score\t-\t$line[7]\t$line[8]\n";
+                }
+            }
+        }
     }
 }
 close(INTRONS) or die("Could not close introns file $introns!\n");
@@ -161,18 +170,31 @@ sub findStrand{
     my $end = shift;
     my $type;
     my $reverse;
+    my $return_type;
+    my $has_plus = 0;
+    my $has_minus = 0;
     if(defined($annos{$seqname})){
         $type = lc(substr($annos{$seqname}, $start-1,2)).lc(substr($annos{$seqname}, $end-2,2));
         $reverse = reverse($type);
         $reverse =~ tr/agct/tcga/;
         foreach (@allowed){
             if($_ eq $type){
-                return "+";
-            }elsif($_ eq $reverse){
-                    return "-";
+                $has_plus = 1;
+            }
+            if($_ eq $reverse){
+                $has_minus = 1;
             }
         }
-        return ".";
+        if( ($has_minus == 1) && ($has_plus == 1) ){
+            $return_type = "b";
+        }elsif($has_minus == 1){
+            $return_type = "-";
+        }elsif($has_plus == 1){
+            $return_type = "+";
+        }else{
+            $return_type = ".";
+        }
+        return $return_type;
     }else{
         print STDERR "WARNING: '$seqname' does not match any sequence in the fasta file. Maybe the two files do not belong together.\n";
         return ".";
