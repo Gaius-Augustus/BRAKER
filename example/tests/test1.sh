f92f00f5613dd0e332b195059d6ff0cd938307f1
Tomas Bruna <bruna.tomas@gmail.com>
2020-07-22 11:14:41 -0400

    Update test1 (with RNA-Seq)

--- example/tests/test1.sh
+++ example/tests/test1.sh
@@ -11,4 +11,8 @@ fi
 # before running this test.
 # Alternatively, you can replace --bam=../RNASeq.bam by --hints=../RNASeq.hints
 
-( time braker.pl --genome=../genome.fa --bam=../RNAseq.bam --softmasking --workingdir=$wd ) &> test1.log
+# --gm_max_intergenic 10000 option is used here only to make the test run faster.
+# It is not recommended to use this option in real BRAKER runs. The speed increase
+# achieved by adjusting this option is negligible on full-sized genomes.
+
+( time braker.pl --genome=../genome2.fa --bam=../RNAseq2.bam --softmasking --workingdir=$wd --cores 8 --gm_max_intergenic 10000) &> test1.log
