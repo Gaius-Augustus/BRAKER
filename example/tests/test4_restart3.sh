7a1649e77055bedea557b808e738d7adb09bba73
Tomas Bruna <bruna.tomas@gmail.com>
2020-08-02 12:13:09 -0400

    Update test4

--- example/tests/test4_restart3.sh
+++ example/tests/test4_restart3.sh
@@ -6,8 +6,8 @@ if [ -d $wd ]; then
 fi
 
 if [ ! -d $oldDir ] ; then
-  echo "ERROR: Directory (with contents) of old BRAKER run $oldDir does not exist, yet. Please run test4.sh before running test4_restart3.sh!"  
+  echo "ERROR: Directory (with contents) of old BRAKER run $oldDir does not exist, yet. Please run test4.sh before running test4_restart3.sh!"
 else
     species=$(cat $oldDir/braker.log | perl -ne 'if(m/AUGUSTUS parameter set with name ([^.]+)\./){print $1;}')
-    ( time braker.pl --genome=../genome.fa --hints=$oldDir/hintsfile.gff --skipAllTraining --species=$species --softmasking --workingdir=$wd --prg=gth ) &> test4_restart3.log
+    ( time braker.pl --genome=../genome2.fa --hints=$oldDir/hintsfile.gff --skipAllTraining --species=$species --softmasking --workingdir=$wd --prg=gth --cores 8 ) &> test4_restart3.log
 fi
