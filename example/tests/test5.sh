b221b81a8ec33bf6e4c7660eb21a8cf8f75695a0
Tomas Bruna <bruna.tomas@gmail.com>
2020-08-05 01:00:07 -0400

    Update test5

--- example/tests/test5.sh
+++ example/tests/test5.sh
@@ -4,5 +4,8 @@ if [ -d $wd ]; then
     rm -r $wd
 fi
 
+# --gm_max_intergenic 10000 option is used here only to make the test run faster.
+# It is not recommended to use this option in real BRAKER runs. The speed increase
+# achieved by adjusting this option is negligible on full-sized genomes.
 
-( time braker.pl --genome=../genome.fa --prot_seq=../prot.fa --prg=gth --bam=../RNAseq.bam --softmasking --workingdir=$wd ) &> test5.log
+( time braker.pl --genome=../genome2.fa --prot_seq=../proteins2.fa --prg=gth --bam=../RNAseq2.bam --softmasking --workingdir=$wd --cores 8 --gm_max_intergenic 10000) &> test5.log
