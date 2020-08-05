9f0027a95cfb48c8234de3872d639900971ac725
Tomas Bruna <bruna.tomas@gmail.com>
2020-08-05 18:41:19 -0400

    Update test3

--- example/tests/test3.sh
+++ example/tests/test3.sh
@@ -4,5 +4,15 @@ if [ -d $wd ]; then
     rm -r $wd
 fi
 
+# Note:
+# The file ../RNASeq2.bam is not contained in the github repository!
+# Make sure that you downloaded this file with
+# wget http://topaz.gatech.edu/GeneMark/Braker/RNAseq2.bam
+# before running this test.
+# Alternatively, you can replace --bam=../RNASeq.bam by --hints=../RNASeq.hints
 
-( time braker.pl --genome=../genome.fa --prot_seq=../orthodb_small.fa --bam=../RNAseq.bam --etpmode --softmasking --workingdir=$wd ) &> test3.log
+# --gm_max_intergenic 10000 option is used here only to make the test run faster.
+# It is not recommended to use this option in real BRAKER runs. The speed increase
+# achieved by adjusting this option is negligible on full-sized genomes.
+
+( time braker.pl --genome=../genome2.fa --prot_seq=../proteins2.fa --bam=../RNAseq2.bam --etpmode --softmasking --workingdir=$wd --cores 8 --gm_max_intergenic 10000 ) &> test3.log
--- example/tests/test3_restart1.sh
+++ example/tests/test3_restart1.sh
@@ -5,8 +5,12 @@ if [ -d $wd ]; then
     rm -r $wd
 fi
 
+# --gm_max_intergenic 10000 option is used here only to make the test run faster.
+# It is not recommended to use this option in real BRAKER runs. The speed increase
+# achieved by adjusting this option is negligible on full-sized genomes.
+
 if [ ! -d $oldDir ] ; then
-  echo "ERROR: Directory (with contents) of old BRAKER run $oldDir does not exist, yet. Please run test3.sh before running test3_restart1.sh!"  
+  echo "ERROR: Directory (with contents) of old BRAKER run $oldDir does not exist, yet. Please run test3.sh before running test3_restart1.sh!"
 else
-    ( time braker.pl --genome=../genome.fa --hints=$oldDir/hintsfile.gff --evidence=$oldDir/evidence.gff --genemark_hintsfile=$oldDir/genemark_hintsfile.gff --etpmode --softmasking --workingdir=$wd ) &> test3_restart1.log
+    ( time braker.pl --genome=../genome2.fa --hints=$oldDir/hintsfile.gff --etpmode --softmasking --workingdir=$wd --cores 8 --gm_max_intergenic 10000) &> test3_restart1.log
 fi
