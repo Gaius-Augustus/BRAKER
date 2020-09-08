d922fde3e031c7f06aa8fe128ec0f1df38d74d58
Katharina Hoff <katharina.hoff@gmail.com>
2020-09-08 15:51:19 +0200

    added test for utr training and prediction with GUSHR

---
+++ example/tests/test9.sh
@@ -0,0 +1,20 @@
+wd=test9
+
+if [ -d $wd ]; then
+    rm -r $wd
+fi
+
+# The expected runtime of this test is ~20 minutes.
+
+# Note:
+# The file ../RNASeq.bam is not contained in the github repository!
+# Make sure that you downloaded this file with
+#    wget http://topaz.gatech.edu/GeneMark/Braker/RNAseq.bam
+# before running this test.
+# Alternatively, you can replace --bam=../RNASeq.bam by --hints=../RNASeq.hints
+
+# --gm_max_intergenic 10000 option is used here only to make the test run faster.
+# It is not recommended to use this option in real BRAKER runs. The speed increase
+# achieved by adjusting this option is negligible on full-sized genomes.
+
+( time braker.pl --genome=../genome.fa --bam=../RNAseq.bam --softmasking --workingdir=$wd --cores 8 --gm_max_intergenic 10000 --UTR=on ) &> test9.log
