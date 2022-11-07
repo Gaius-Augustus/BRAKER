129845327f28e13daf4a6fa03143ffcb557c6b2a
Lars Gabriel <gabriell@login-a.maas>
2022-11-07 15:32:37 +0000

    correcting test3 restart scripts

--- example/tests/test3_restart3.sh
+++ example/tests/test3_restart3.sh
@@ -9,5 +9,5 @@ if [ ! -d $oldDir ] ; then
   echo "ERROR: Directory (with contents) of old BRAKER run $oldDir does not exist, yet. Please run test3.sh before running test3_restart3.sh!"
 else
     species=$(cat $oldDir/braker.log | perl -ne 'if(m/AUGUSTUS parameter set with name ([^.]+)\./){print $1;}')
-    ( time braker.pl --genome=../genome.fa --hints=$oldDir/hintsfile.gff --skipAllTraining --species=$species --workingdir=$wd --threads 8 --skipOptimize ) &> test3_restart3.log
+    ( time braker.pl --genome=../genome.fa --hints=$oldDir/hintsfile.gff --skipAllTraining --genemarkgtf=$oldDir/GeneMark-ETP/genemark.gtf --traingenes=$oldDir/GeneMark-ETP/training.gtf --species=$species --workingdir=$wd --threads 8 --skipOptimize ) &> test3_restart3.log
 fi
