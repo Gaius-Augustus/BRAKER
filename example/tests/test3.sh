583da3c0050206610285c45f724a248b0401a398
KatharinaHoff <katharina.hoff@gmail.com>
2022-11-07 13:06:18 +0100

    removing explicit flags for epmode etpmode

--- example/tests/test3.sh
+++ example/tests/test3.sh
@@ -20,4 +20,4 @@ fi
 # For instructions on how to prepare the proteins.fa input file from OrthoDB,
 # see https://github.com/gatech-genemark/ProtHint#protein-database-preparation
 
-( time braker.pl --genome=../genome.fa --prot_seq=../proteins.fa --bam=../RNAseq.bam --etpmode --softmasking --workingdir=$wd --cores 8) &> test3.log
+( time braker.pl --genome=../genome.fa --prot_seq=../proteins.fa --bam=../RNAseq.bam --softmasking --workingdir=$wd --cores 8) &> test3.log
