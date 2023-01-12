77ea87ce5308bc6362525c7b00d406815ff8cd75
KatharinaHoff <katharina.hoff@gmail.com>
2023-01-12 14:00:57 +0000

    BRAKER2 test

---
+++ example/singularity-tests/test2.sh
@@ -0,0 +1,23 @@
+# Copy this script into the folder where you want to execute it prior modification and
+# prior running it, e.g.:
+# singularity exec -B $PWD:$PWD /opt/BRAKER/example/singularity-tests/test1.sh .
+
+# Next, modifiy the ETP folder time to your system
+ETP=${HOME}/ETP
+
+wd=test2
+
+if [ -d $wd ]; then
+    rm -r $wd
+fi
+
+# The expected runtime of this test is ~20 minutes.
+
+# --gm_max_intergenic 10000 option is used here only to make the test run faster.
+# It is not recommended to use this option in real BRAKER runs. The speed increase
+# achieved by adjusting this option is negligible on full-sized genomes.
+
+singularity exec -B $PWD:$PWD braker3.sif braker.pl --genome=/opt/BRAKER/example/genome.fa --prot_seq=/opt/BRAKER/example/proteins.fa --softmasking --workingdir=${wd} --threads 8 --GENEMARK_PATH=${ETP}/bin/gmes --PROTHINT_PATH=${ETP}/bin/gmes/ProtHint/bin \
+	    --gm_max_intergenic 10000 --skipOptimize
+
+
