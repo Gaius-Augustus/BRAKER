b878e64d5a00ac30ad6906e16adc9639304893d2
KatharinaHoff <katharina.hoff@gmail.com>
2024-01-06 16:38:53 +0100

    including compleasm test

--- example/docker-tests/test1.sh
+++ example/docker-tests/test1.sh
@@ -8,8 +8,11 @@ fi
 
 # --gm_max_intergenic 10000 option is used here only to make the test run faster.
 # It is not recommended to use this option in real BRAKER runs. The speed increase
-# achieved by adjusting this option is negligible on full-sized genomes.
+# achieved by adjusting this option is negligible on full-sized genomes. Also,
+# --skipOptimize is used here only to make the test run faster.
+# Use an appropriate BUSCO lineage in real life use case. eukaryota_odb10 is used here
+# only to make the test run faster.
 
 export GENEMARK_PATH=$GENEMARK_PATH/gmes
 
-( time braker.pl --genome=/opt/BRAKER/example/genome.fa --bam=/opt/BRAKER/example/RNAseq.bam --workingdir=$wd --threads 8 --gm_max_intergenic 10000 --skipOptimize ) &> test1.log
+( time braker.pl --genome=/opt/BRAKER/example/genome.fa --bam=/opt/BRAKER/example/RNAseq.bam --workingdir=$wd --threads 8 --gm_max_intergenic 10000 --skipOptimize --busco_lineage eukaryota_odb10 ) &> test1.log
--- example/docker-tests/test2.sh
+++ example/docker-tests/test2.sh
@@ -8,11 +8,14 @@ fi
 
 # --gm_max_intergenic 10000 option is used here only to make the test run faster.
 # It is not recommended to use this option in real BRAKER runs. The speed increase
-# achieved by adjusting this option is negligible on full-sized genomes.
+# achieved by adjusting this option is negligible on full-sized genomes. Also,
+# --skipOptimize is used here only to make the test run faster.
+# Use an appropriate BUSCO lineage in real life use case. eukaryota_odb10 is used here
+# only to make the test run faster.
 
 # For instructions on how to prepare the proteins.fa input file from OrthoDB,
 # see https://github.com/gatech-genemark/ProtHint#protein-database-preparation
 
 export GENEMARK_PATH=$GENEMARK_PATH/gmes
 
-( time braker.pl --genome=/opt/BRAKER/example/genome.fa --prot_seq=/opt/BRAKER/example/proteins.fa --workingdir=$wd --threads=8 --gm_max_intergenic 10000 --skipOptimize ) &> test2.log
+( time braker.pl --genome=/opt/BRAKER/example/genome.fa --prot_seq=/opt/BRAKER/example/proteins.fa --workingdir=$wd --threads=8 --gm_max_intergenic 10000 --skipOptimize --busco_lineage eukaryota_odb10 ) &> test2.log
